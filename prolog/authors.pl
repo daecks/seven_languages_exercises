b90a49defee5d78e7fb8e68ec4aa86884fc92a03
Kristian Golding <a18887@motorolasolutions.com>
2016-02-24 16:26:18 -0500

    adding more prolog to day 2

---
+++ prolog/authors.pl
@@ -0,0 +1,15 @@
+book(hitchhikers_guide_to_the_galaxy).
+book(how_the_mind_works).
+book(the_martian).
+book(magician).
+book(a_darkness_at_sethanon).
+
+author(douglas_adams).
+author(steven_pinker).
+author(raymond_feist).
+
+wrote(douglas_adams, hitchhikers_guide_to_the_galaxy).
+wrote(steven_pinker, how_the_mind_works).
+wrote(raymond_feist, a_darkness_at_sethanon).
+wrote(raymond_feist, magician).
+
---
+++ prolog/musicians.pl
@@ -0,0 +1,10 @@
+musician_instrument(bellamy, guitar).
+musician_instrument(bellamy, piano).
+musician_instrument(gaga, piano).
+musician_instrument(chopin, piano).
+
+musician_genre(bellamy, alternative).
+musician_genre(bellamy, rock).
+musician_genre(gaga, pop).
+musician_genre(chopin, classical).
+
